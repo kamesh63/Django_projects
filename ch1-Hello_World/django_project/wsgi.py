1df026f6850dbe00ea928cd86f80274b3da469fb
kamesh63 <146455043+kamesh63@users.noreply.github.com>
2025-12-17 17:14:16 +0530

    Enhance wsgi.py with additional comments
    Added comments to explain the purpose of wsgi.py.

--- ch1-Hello_World/django_project/wsgi.py
+++ ch1-Hello_World/django_project/wsgi.py
@@ -3,6 +3,9 @@ WSGI config for django_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
+wsgi.py	configures	a	WSGI	(Web	Server	Gateway	Interface)	application,
+the	default	setting	for	Django
+
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
