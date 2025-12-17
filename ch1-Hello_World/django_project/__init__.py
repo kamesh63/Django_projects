23a94da91bfbe36fa9de0b4f710a9f61b6fde7f8
kamesh63 <146455043+kamesh63@users.noreply.github.com>
2025-12-17 17:11:34 +0530

    Enhance __init__.py with package explanation
    Added explanation about __init__.py and its role in Python packages.

--- ch1-Hello_World/django_project/__init__.py
+++ ch1-Hello_World/django_project/__init__.py
@@ -1,1 +1,3 @@
-This folder is a Python package.
+__init__.py	indicates	that	the	files	in	the	folder	are	part	of	a	Python
+package.	Without	this	file,	we	cannot	import	files	from	another	directory,
+which	we	will	do	often	in	Django!
