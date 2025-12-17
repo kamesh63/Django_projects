302fbdeea7d51d6a32ae79a1fe5bd245c04c8231
kamesh63 <146455043+kamesh63@users.noreply.github.com>
2025-12-17 17:35:16 +0530

    Add comment about models.py purpose
    Added a comment explaining the purpose of models.py.

--- ch1-Hello_World/pages/models.py
+++ ch1-Hello_World/pages/models.py
@@ -1,4 +1,5 @@
 # What data you store and how it is stored
+# models.py	is	where	we	define	our	database	models,	which	Django automatically	translates	into	database	tables
 from django.db import models
 
 # Create your models here.
